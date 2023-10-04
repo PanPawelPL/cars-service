e2924ef04d72c5bc39aa1130c5fa7e3441b8f491
Pan Pawel <project.panpawel@gmail.com>
2023-10-04 11:21:16 +0200

    Added optimalization imports from cars module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CarsModule } from './cars/cars.module';
-import { CarsService } from './cars/cars.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
-import { CarsRoutingModule } from './cars/cars-routing.modules';
+
+import { CarsModule, CarsService, CarsRoutingModule } from './cars/index';
 
 @NgModule({
   declarations: [
